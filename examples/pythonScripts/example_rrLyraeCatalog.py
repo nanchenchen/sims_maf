b7963d1d66c1834a3d053a1496ec964f16d652ea
danielsf <scott.f.daniel@gmail.com>
2014-11-20 19:47:57 -0800

    can get the MJDs of observation for all objects in our query
    Former-commit-id: 0de69efe977621b029ef2d50ddd73ab9793ba085

--- examples/pythonScripts/example_rrLyraeCatalog.py
+++ examples/pythonScripts/example_rrLyraeCatalog.py
@@ -5,33 +5,8 @@ import lsst.sims.maf.db as db
 from lsst.sims.catalogs.generation.db import CatalogDBObject, ObservationMetaData
 from lsst.sims.catUtils.baseCatalogModels import *
 
-from lsst.sims.photUtils import PhotometryStars
+from lsst.sims.photUtils import PhotometryStars, Variability
 from lsst.sims.catalogs.measures.instance.fileMaps import defaultSpecMap
-
-#initialize baseline (i.e. not variable) photometry
-photObj = PhotometryStars()
-bandPassList = ['u', 'g', 'r', 'i', 'z', 'y']
-photObj.loadBandPassesFromFiles(bandPassList)
-photObj.setupPhiArray_dict()
-
-rrLyraeDB = CatalogDBObject.from_objid('rrlystars')
-obs_metadata = ObservationMetaData(unrefractedRA=0.0, unrefractedDec=0.0,
-                                   boundType='circle', boundLength=4.0)
-
-colNames = ['raJ2000', 'decJ2000','varParamStr','magNorm','sedFilename']
-dtype = numpy.dtype([('id',int),('raJ2000',float),('decJ2000',float),
-                     ('varParamStr',str,256),('magNorm',float),('sedFilename',str,100)])
-
-rrLyraeDB.dtype = dtype
-
-results=rrLyraeDB.query_columns(colnames=colNames, obs_metadata=obs_metadata,
-                                returnRecArray=True)
-for chunk in results:
-    sedNames = chunk['sedFilename']
-    magNorms = chunk['magNorm']
-    sedList = photObj.loadSeds(sedNames, magNorm=magNorms, specFileMap=defaultSpecMap)
-    baselineMagnitudes = photObj.calculate_magnitudes(sedList)
-
 
 # Connect to opsim
 dbAddress = 'sqlite:///../../../garage/OpSimData/opsimblitz2_1060_sqlite.db'
@@ -40,14 +15,50 @@ colnames = ['expMJD', 'fieldRA', 'fieldDec']
 sqlconstraint ='filter="r"'
 # Get opsim simulation data
 simdata = oo.fetchMetricData(colnames, sqlconstraint)
-# Init the slicer, set 2 points
-slicer = slicers.UserPointsSlicer(ra=[0., .1], dec=[0., -.1])
-# Setup slicer (builds kdTree)
-slicer.setupSlicer(simdata)
-# Slice Point for index zero
-ind = slicer._sliceSimData(0)
-expMJDs = simdata[ind['idxs']]['expMJD']
-# Find the expMJDs for the 2nd point
-ind = slicer._sliceSimData(1)
-expMJDs = simdata[ind['idxs']]['expMJD']
+
+#initialize baseline (i.e. not variable) photometry
+photObj = PhotometryStars()
+bandPassList = ['u', 'g', 'r', 'i', 'z', 'y']
+photObj.loadBandPassesFromFiles(bandPassList)
+photObj.setupPhiArray_dict()
+
+varObj = Variability()
+
+rrLyraeDB = CatalogDBObject.from_objid('rrlystars')
+obs_metadata = ObservationMetaData(unrefractedRA=0.0, unrefractedDec=0.0,
+                                   boundType='circle', boundLength=2.0)
+
+colNames = ['raJ2000', 'decJ2000','varParamStr','magNorm','sedFilename','distance']
+dtype = numpy.dtype([('id',int),('raJ2000',float),('decJ2000',float),
+                     ('varParamStr',str,256),('magNorm',float),('sedFilename',str,100),
+                     ('distance',float)])
+
+rrLyraeDB.dtype = dtype
+
+results = rrLyraeDB.query_columns(colnames=colNames, obs_metadata=obs_metadata,
+                                returnRecArray=True)
+
+for metaData in results:
+    sedNames = metaData['sedFilename']
+    magNorms = metaData['magNorm']
+    sedList = photObj.loadSeds(sedNames, magNorm=magNorms, specFileMap=defaultSpecMap)
+    baselineMagnitudes = photObj.calculate_magnitudes(sedList)
+
+    # Init the slicer, set 2 points
+    slicer = slicers.UserPointsSlicer(ra=metaData['raJ2000'], dec=metaData['decJ2000'])
+    # Setup slicer (builds kdTree)
+    slicer.setupSlicer(simdata)
+    
+    for i,vps in enumerate(metaData['varParamStr']):
+        # Slice Point for index zero
+        ind = slicer._sliceSimData(i)
+        expMJDs = simdata[ind['idxs']]['expMJD']
+        print expMJDs
+        
+    
+    
+    
+    # Find the expMJDs for the 2nd point
+    ind = slicer._sliceSimData(1)
+    expMJDs = simdata[ind['idxs']]['expMJD']
 

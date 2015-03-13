7794571b7dbf27ec35a922f37ca40b297a128486
Peter Yoachim <yoachim@uw.edu>
2015-03-13 10:34:22 -0700

    starting test
    Former-commit-id: 83b5d84c640877ef95084d6beff823b346e65e0a

---
+++ tests/testHpDust.py
@@ -0,0 +1,15 @@
+import numpy as np
+import unittest
+from lsst.sims.photUtils import EBV
+from lsst.sims.maf.maps import EBVhp
+
+
+class TestDustMaps(unittest.TestCase):
+
+    def testDust(self):
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()

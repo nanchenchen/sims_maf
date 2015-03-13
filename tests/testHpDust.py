1ec5f61704bfb121fafeb5ffc8cc22660099a62b
Peter Yoachim <yoachim@uw.edu>
2015-03-13 10:34:42 -0700

    actually save
    Former-commit-id: a6ac7aaa5466ff42c7577c59a86ca1748e49f4af

--- tests/testHpDust.py
+++ tests/testHpDust.py
@@ -7,7 +7,18 @@ from lsst.sims.maf.maps import EBVhp
 class TestDustMaps(unittest.TestCase):
 
     def testDust(self):
+        np.random.seed(42)
+        ra = np.random.rand(100)*2*np.pi
+        dec = np.random.rand(100)*np.pi - np.pi/2.
 
+        nside = 512
+
+        hpVals = EBVhp(nside, ra=ra, dec=dec, interp=False)
+
+        dustmap = EBV.EBVbase()
+        dustmap.load_ebvMapNorth()
+        dustmap.load_ebvMapSouth()
+        schlegVals = dustmap.calculateEbv(np.array([ra,dec]), interp=False)
 
 
 
